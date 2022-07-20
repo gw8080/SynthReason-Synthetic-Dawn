4d9c8ad51d2b30ee096badc4a6560e1eca2da60d
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-07-20 11:29:46 +1000

    Update SynthReason interactive.py

--- SynthReason interactive.py
+++ SynthReason interactive.py
@@ -1,7 +1,7 @@
-# SynthReason - Synthetic Dawn - intelligent symbolic manipulation
+#SynthReason - Synthetic Dawn - Expert knowledge system
 # BSD 2-Clause License
 # 
-# Copyright (c) 2022, GeorgeFW1101 - George Wagenknecht
+# Copyright (c) 2022, gw8080 - George Wagenknecht
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -26,36 +26,69 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import random
 import re
+import math
 size = 100
 targetNgramSize = 3
-spread = 3
-def index_of(val, in_list):
-    try:
-        return in_list.index(val)
-    except ValueError:
-        return -1
+spread = 1
+entropy = 4.5
+def processB(data,file):     
+        with open(file, encoding='UTF-8') as f:
+            text = f.read()
+        ini = random.randint(0,len(data))
+        string = returnWords(data,ini,targetNgramSize)
+        ini = random.randint(0,len(data))
+        stringX = returnWords(data,ini,targetNgramSize)
+        words = convert(string)
+        x = 0
+        for word in words:
+            if x < len(word):
+                totalA = ""
+                while(x < round(len(word)/entropy)):
+                    totalA += word[x]
+                    x+=1
+                x = round(len(word)/entropy)
+                totalB = ""
+                while(x < len(word)):
+                    totalB += word[x]
+                    x+= 1
+                if string.find(" " + stringX + " ") > -1 or string.find(" " + totalA + " ") == -1:
+                    return string
 def convert(lst):
     return (lst.split())
 def formatSentences(sync):
     sentences = sync.split('.')
     i = 0
     total = ""
-    while(i < len(sentences)-1):
+    if(i < len(sentences)-1):
         total += sentences[i] + "."
-        i += 1
+        return sentences[i] + "."
     return total
 def process(user,file):
-    with open(file, encoding='ISO-8859-1') as f:
+    with open(file, encoding='UTF-8') as f:
         text = f.read()
     sentences = text.split('.')
     output = ""
-    sentences = sorted(sentences)
-    for line in sentences:
-        words = convert(user)
+    i = 1
+    while(i < len(sentences)-2):
+        words = convert(sentences[i])
         for word in words:
-            if line.find(" " + word + " ") > -1:
-                output += line + ". "
-                break
+            x = 0
+            total = ""
+            while(x < round(len(word)/entropy)):
+                total += word[x]
+                x+=1
+            stringA = sentences[i][sentences[i].find(total):sentences[i].find(" ",sentences[i].find(total)+1)]
+            x = round(len(word)/entropy)
+            total = ""
+            while(x < len(word)):
+                total += word[x]
+                x+=1
+            stringB = sentences[i][sentences[i].find(total):sentences[i].find(" ",sentences[i].find(total)+1)]
+            if len(sentences[i]) > 0:
+                if user.find(" " + stringA + " ") > -1 or user.find(" " + stringB + " ") > -1:
+                    output += sentences[i] + "."
+                    break
+        i+=1
     return output
 def returnWords(dataX,pos,length):
     ngram = ""
@@ -64,64 +97,42 @@ def returnWords(dataX,pos,length):
         ngram += dataX[pos+n] + " "
         n+=1
     return ngram
-with open("fileList.conf", encoding='ISO-8859-1') as f:
+with open("fileList.conf", encoding='UTF-8') as f:
     files = f.readlines()
     print("SynthReason - Synthetic Dawn")
+    with open("questions.conf", encoding='UTF-8') as f:
+    	questions = f.readlines()
     filename = "Compendium#" + str(random.randint(0,10000000)) + ".txt"
+    random.shuffle(questions)
     while(True):
         print()
         user = re.sub('\W+',' ',input("USER: "))
         stat = 0
         x = 0
-        counter = 0
-        counterB = 0
-        while(stat < len(convert(user)) and counterB < len(files)):
-            stat = 0
-            counterB += 1
-            random.shuffle(files)
-            for file in files: 
-                sync = ""
-                data = convert(process(user,file.strip()))
-                db = ''.join(data)
+        random.shuffle(files)
+        for file in files: 
+            counter = 0
+            data = convert(process(user,file.strip()))
+            sync = ""
+            while(counter < size):
+                counter += 1
+                stat = 0
                 if len(data) > 100:
-                    n = 1
-                    counter = 0
-                    prevA = 0
-                    prevB = 0
-                    n = index_of((convert(user)[random.randint(0,len(convert(user))-1)]),data)+1
-                    while(n < len(data) and n > 0):
-                        string = returnWords(data,n,random.randint(targetNgramSize,targetNgramSize*random.randint(1,2)))
-                        stringB = returnWords(data,random.randint(1,len(data)),random.randint(1,targetNgramSize))
-                        if string.find("a") + string.find("e") + string.find("i") + string.find("o") + string.find("u") == stringB.find("b") + stringB.find("c") + stringB.find("d") + stringB.find("f") + stringB.find("g") + stringB.find("h") + stringB.find("j") + stringB.find("k") + stringB.find("l") + stringB.find("m") + stringB.find("n") + stringB.find("p") + stringB.find("q") + stringB.find("r") + stringB.find("s") + stringB.find("t") + stringB.find("v") + stringB.find("w") + stringB.find("x") + stringB.find("y") + stringB.find("z"):
-                            if sync.find(stringB) == -1:
-                                sync += stringB
-                                n+=1
-                        counter += 1
-                        if counter > 100:  
-                            counter = 0
-                            n+=1
-                        if len(convert(sync)) >= size:
+                    ini = 1
+                    sync += processB(data,file.strip())
+                    if len(convert(sync)) >= size and sync.find(".") > -1:
                             break
-                    stat = 0
-                    words = convert(user)
-                    for word in words:
-                        if sync.find(" " + word + " " ) > -1 and len(word) > 0:
-                            stat+=1
-                sync = formatSentences(sync)
-                if len(convert(sync)) > size and stat > len(convert(user))/2.5:
-                    print()                
-                    print("using" ,file.strip(), "answering:", user)
-                    print("AI:" ,sync)
-                    f = open(filename, "a", encoding="utf8")
-                    f.write("\n")
-                    f.write("using " + file.strip() + " answering: " + user)
-                    f.write("\n")
-                    f.write(sync)
-                    f.write("\n")
-                    f.write("\n")
-                    f.close()
-                    x+=1
-                    if x >= spread:
-                        break
+            print()
+            syncB = formatSentences(sync)
+            words = convert(syncB)  
+            print("AI:" ,syncB)
+            f = open(filename, "a", encoding="utf8")
+            f.write("\n")
+            f.write("using " + file.strip() + " answering: " + user)
+            f.write("\n")
+            f.write(syncB)
+            f.write("\n")
+            f.close()
+            x+=1
             if x >= spread:
                 break
